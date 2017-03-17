bc7611fc80fb33239cf4b92f533cc769c9057f4c
AlejandroHerr <alejandrohnc88@gmail.com>
2017-03-17 14:47:06 +0100

    Two canvas, better buttons

--- src/js/containers/DemoApp.js
+++ src/js/containers/DemoApp.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import CSSModules from 'react-css-modules';
 import 'basscss/css/basscss.css';
 import Chart from '../react-d3/components/Chart';
 import LegendGroup from '../react-d3/components/LegendGroup';
@@ -8,38 +9,52 @@ import { GeometryRecord } from '../react-d3/models';
 import { rrdFetchAction, viewsFetchAction } from '../actions/fetcher';
 import { unselectShape, selectShape } from '../actions/charts';
 import { selectViews, selectCharts, selectView, selectSelected } from '../selectors';
+import styles from './style.css';
+
+const Button = ({ view, fetch, canvas, styles }) => (
+  <button
+    className="btn not-rounded"
+    onClick={() => fetch(view.get('id'), view.get('modules'), canvas)}
+    className={styles.btn}
+  >
+    {view.get('name')}
+  </button>
+  );
+const ViewButtons = ({ views, fetch, canvas, styles }) => (
+  <div className="clearfix">
+    {views.map(view => (
+      <div className="col m1" key={`button${view.get('id')}`}>
+        <Button canvas={canvas} fetch={fetch} styles={styles} view={view} />
+      </div>))}
+  </div>);
 
 class DemoApp extends PureComponent {
   componentDidMount() {
     this.props.fetchViews();
   }
   render() {
-    const { data, geometry, fetch, views, mainCharts, scale, chartViews, mainSelected, mainView, mouseOver, mouseOut } = this.props;
-    console.log(mainCharts);
+    const { styles, asideCharts, asideSelected, asideView, data, main, aside, fetch, views, mainCharts, scale, chartViews, mainSelected, mainView, mouseOver, mouseOut } = this.props;
+
     return (
       <div>
-        <div>
-          {views.map(view => (
-            <button key={view.get('id')} onClick={() => fetch(view.get('id'), view.get('modules'), 'main')}>
-              {view.get('name')}
-            </button>),
-          )}
-        </div>
-        <div>
-          <svg height={geometry.height} width={geometry.width}>
-            {mainCharts.map((chart, idx) => (
-              <Chart
-                canvas={'main'}
-                chart={chart}
-                geometry={geometry}
-                key={`chart_main_${chart.id}`}
-                mouseOut={mouseOut}
-                mouseOver={mouseOver}
-                selected={mainSelected.get(chart.id, null)}
-                view={mainView.charts.get(idx)}
-              />),
+        <div className="col col-7">
+          <ViewButtons canvas={'main'} fetch={fetch} styles={styles} views={views} />
+          <div>
+            <svg height={main.height} width={main.width}>
+              {mainCharts.map((chart, idx) => (
+                <Chart
+                  canvas={'main'}
+                  chart={chart}
+                  geometry={main}
+                  key={`chart_main_${chart.id}`}
+                  mouseOut={mouseOut}
+                  mouseOver={mouseOver}
+                  selected={mainSelected.get(chart.id, null)}
+                  view={mainView.charts.get(idx)}
+                />),
             )}
-          </svg>
+            </svg>
+          </div>
           { mainCharts && mainCharts.size ? (
             <LegendGroup
               canvas={'main'}
@@ -51,25 +66,57 @@ class DemoApp extends PureComponent {
           null
           }
         </div>
+        <div className="col col-4">
+          <ViewButtons canvas={'aside'} fetch={fetch} styles={styles} views={views} />
+          <div>
+            <svg height={aside.height} width={aside.width}>
+              {asideCharts.map((chart, idx) => (
+                <Chart
+                  canvas={'aside'}
+                  chart={chart}
+                  geometry={aside}
+                  key={`chart_aside_${chart.id}`}
+                  mouseOut={mouseOut}
+                  mouseOver={mouseOver}
+                  selected={asideSelected.get(chart.id, null)}
+                  view={asideView.charts.get(idx)}
+                />),
+            )}
+            </svg>
+          </div>
+        </div>
       </div>
     );
   }
 }
-const geometry = new GeometryRecord({
-  height: 450,
-  width: 800,
+const main = new GeometryRecord({
+  height: 400,
+  width: 700,
   marginTop: 10,
   marginRight: 50,
   marginBottom: 30,
   marginLeft: 50,
 });
 
-const mapStateToProps = (state, ownProps = { geometry }) => ({
-  geometry: ownProps.geometry,
+const aside = new GeometryRecord({
+  height: 200,
+  width: 400,
+  marginTop: 10,
+  marginRight: 50,
+  marginBottom: 30,
+  marginLeft: 50,
+});
+
+const mapStateToProps = (state, ownProps = { aside, main }) => ({
+  main: ownProps.main,
+  aside: ownProps.aside,
   views: selectViews(state),
   mainCharts: selectCharts(state, 'main'),
   mainView: selectView(state, 'main'),
   mainSelected: selectSelected(state, 'main'),
+  asideCharts: selectCharts(state, 'aside'),
+  asideView: selectView(state, 'aside'),
+  asideSelected: selectSelected(state, 'aside'),
 
 });
 const mapDispatchToProps = dispatch => ({
@@ -79,7 +126,7 @@ const mapDispatchToProps = dispatch => ({
   mouseOut: bindActionCreators(unselectShape, dispatch),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(DemoApp);
+export default connect(mapStateToProps, mapDispatchToProps)(CSSModules(DemoApp, styles));
 /* <LegendGroup
             canvas={'main'}
             charts={mainCharts}
